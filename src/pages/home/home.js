e5a5cb2f4c34fbc2048118963a273176c3b55d55
CiaoFella <ciao@julianfella.de>
2025-03-15 16:17:34 +0100

    Add animation logging to home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,9 +3,12 @@ import { gsap, ScrollTrigger } from "../../vendor";
 (function () {
   function init() {
     console.log("Home page animations initialized");
+    animateText();
   }
 
-  function animateText() {}
+  function animateText() {
+    console.log("animateText");
+  }
 
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", init);
